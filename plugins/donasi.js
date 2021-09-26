dd9c2abd1bad2930d987f822c66575b63edf08d2
lucasalchapar <54723464+lucasalchapar@users.noreply.github.com>
2021-09-26 23:49:23 +0200

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
-╰────
 
-╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
+╭─「 Donasi • Naco 」
+│ • https://paypal.me/lucasalchapar?locale.x=es_ES
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
